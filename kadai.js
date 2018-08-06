be3e575481e6108aa83d5fa95d62f6577511b3a5
zm17494 <zm17494@josai.ac.jp>
2018-08-06 03:40:59 -0400

    message

---
+++ kadai.js
@@ -0,0 +1,29 @@
+//alert('');
+// alert('Hello, Yuza!');
+//alert(2+3);
+//alert(Math.sqrt(1+1));
+//function imgClick() {
+  //alert("クリックされました");
+//}
+
+         
+
+function showimg1(){
+document.area1.src = "1nenn.jpg";
+}
+
+function showimg2(){
+document.area1.src = "2nenn.jpg";
+}
+
+function showimg3(){
+document.area1.src = "3nenn.jpg";
+}
+
+function showimg4(){
+document.area1.src = "4nenn.jpg";
+}
+            
+function showimg5(){
+document.area1.src = "zenntai.jpg";
+}
--- myjs.js
+++
@@ -1,4 +0,0 @@
-alert('Hello,　Yuza!');
-// alert('Hello, Yuza!');
-alert(2+3);
-alert(Math.sqrt(1+1));
